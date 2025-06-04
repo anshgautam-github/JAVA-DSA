060af2f33c612b63f2863622332d75e28b54159a
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-06-04 12:29:35 +0530

    Update 01-Delete all occurrences of a key in DLL.java

--- LinkedList/DLL Problems/01-Delete all occurrences of a key in DLL.java
+++ LinkedList/DLL Problems/01-Delete all occurrences of a key in DLL.java
@@ -1,1 +1,36 @@
 
+class Solution {
+    public ListNode deleteAllOccurrences(ListNode head, int target) {
+          ListNode temp = head;
+
+        while (temp != null) {
+            if (temp.val == target) {
+                // Update head if needed
+                if (temp == head) {
+                    head = temp.next;
+                }
+
+                ListNode nextNode = temp.next;
+                ListNode prevNode = temp.prev;
+
+                // Update previous node's next
+                if (nextNode != null) {
+                    nextNode.prev = prevNode;
+                }
+
+                // Update next node's previous
+                if (prevNode != null) {
+                    prevNode.next = nextNode;
+                }
+
+                // Delete the current node
+                temp = nextNode;
+            } else {
+                temp = temp.next;
+            }
+        }
+
+        return head;
+    }
+}
+
