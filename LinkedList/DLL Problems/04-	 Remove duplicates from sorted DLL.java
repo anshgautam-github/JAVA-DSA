c86c73140f670d4e977fccc79b0bd7160fed6297
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-06-04 13:01:32 +0530

    Update 04-	 Remove duplicates from sorted DLL.java

--- LinkedList/DLL Problems/04-	 Remove duplicates from sorted DLL.java
+++ LinkedList/DLL Problems/04-	 Remove duplicates from sorted DLL.java
@@ -1,1 +1,19 @@
 
+class Solution {
+    public ListNode removeDuplicates(ListNode head) {
+        ListNode temp=head;
+        while(temp!=null && temp.next!=null){
+            ListNode nextNode=temp.next;
+            while(nextNode!=null && nextNode.val==temp.val){
+                 nextNode = nextNode.next;
+            }
+            temp.next=nextNode;
+            if (nextNode != null) {
+                nextNode.prev = temp;
+            }
+             temp=temp.next;
+        }
+        return head;
+       
+    }
+}
