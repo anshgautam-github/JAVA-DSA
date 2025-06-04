5be794239d65d9ead49f758cbeff69a16cb59c5f
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-06-04 12:38:44 +0530

    Update 03-Find pairs with given sum in DLL.java

--- LinkedList/DLL Problems/03-Find pairs with given sum in DLL.java
+++ LinkedList/DLL Problems/03-Find pairs with given sum in DLL.java
@@ -1,1 +1,27 @@
 
+class Solution {
+    public static ArrayList<ArrayList<Integer>> findPairsWithGivenSum(int target,
+                                                                      Node head) {
+        // code here
+        ArrayList<ArrayList<Integer>> res=new ArrayList<>();
+        Node first=head;
+        Node second=head;
+        while(second.next!=null) second=second.next;
+        
+        while(first.data<second.data){
+            if(first.data+second.data == target){
+                ArrayList<Integer>pair=new ArrayList<>();
+                pair.add(first.data);
+                pair.add(second.data);
+                res.add(pair);
+                first=first.next;
+                second=second.prev;
+            }else if(first.data+second.data < target){
+                first=first.next;
+            }else{
+                second=second.prev;
+            }
+        }
+        return res;
+    }
+}
