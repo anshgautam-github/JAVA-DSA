3fb54cf9cb9e9854ac0e18d2bc166bb4e38aa489
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-06-12 22:47:09 +0530

    Update 09-Remove Nth Node from End of LL.java

--- LinkedList/SLL Problems/09-Remove Nth Node from End of LL.java
+++ LinkedList/SLL Problems/09-Remove Nth Node from End of LL.java
@@ -21,3 +21,34 @@ class Solution {
 
     }
 }
+
+
+/*
+
+ public ListNode removeNthFromEnd(ListNode head, int n) {
+       if (head == null) {
+            return null;
+        }
+        int cnt = 0;
+        ListNode temp = head;
+
+        while (temp != null) {
+            cnt++;
+            temp = temp.next;
+        }
+        if (cnt == n) {
+            return head.next;
+        }
+        temp=head;
+        int len=cnt-n;
+        for(int i=0;i<len-1;i++){
+            temp=temp.next;
+        }
+
+        
+        // Delete the Nth node from the end
+        temp.next = temp.next.next;
+        return head;
+    }   
+
+*/
