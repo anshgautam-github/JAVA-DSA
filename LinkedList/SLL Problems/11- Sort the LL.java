42f0d1a6395ab2fa0f14578684bd3b323732da4e
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-06-12 22:55:02 +0530

    Update 11- Sort the LL.java

--- LinkedList/SLL Problems/11- Sort the LL.java
+++ LinkedList/SLL Problems/11- Sort the LL.java
@@ -1,1 +1,15 @@
-
+    public ListNode sortList(ListNode head) {
+        List<Integer> ls=new ArrayList<>();
+        ListNode temp=head;
+        while(temp!=null){
+            ls.add(temp.val);
+            temp=temp.next;
+        }
+        Collections.sort(ls);
+        temp=head;
+        for(int i=0; i<ls.size(); i++){
+            temp.val=ls.get(i);
+            temp=temp.next;
+        }
+        return head;
+    }
