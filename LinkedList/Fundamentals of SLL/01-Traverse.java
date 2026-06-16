562f088a37fbfc7b56d9b4acd093a6cd52e6b2eb
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-06-16 14:57:44 +0530

    Update 01-Traverse.java

--- LinkedList/Fundamentals of SLL/01-Traverse.java
+++ LinkedList/Fundamentals of SLL/01-Traverse.java
@@ -1,36 +1,15 @@
-import java.util.*;
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-        val = 0;
-        next = null;
-    }
-
-    ListNode(int data1) {
-        val = data1;
-        next = null;
-    }
-
-    ListNode(int data1, ListNode next1) {
-        val = data1;
-        next = next1;
+class Solution {
+    public List<Integer> LLTraversal(ListNode head) {
+        //your code goes here
+        ListNode temp = head;
+        List<Integer> ans = new ArrayList<>();
+        while(temp!=null){
+            ans.add(temp.data);
+            temp=temp.next;
+        }
+        return ans;
     }
 }
- 
-class Traverse {
-  
-    public List<Integer> LLTraversal(ListNode head) {
-        
-        ListNode temp=head;
-        List<Integer>res=new ArrayList<>();
-        while(temp!=null){
-            res.add(temp.val); //add function -> accepts an integer -> so temp.val
-            temp=temp.next;  ////first we add then move to the next element 
-            
-        }
-        return res;
-        
-    }
-}   
+
+// TC : O(n)
+// SC : O(1)
