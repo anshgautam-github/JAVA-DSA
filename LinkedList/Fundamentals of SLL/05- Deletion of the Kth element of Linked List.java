3326c72d7dda274ce2a9948520b3392a506928c5
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-06-16 17:10:37 +0530

    Update 05- Deletion of the Kth element of Linked List.java

--- LinkedList/Fundamentals of SLL/05- Deletion of the Kth element of Linked List.java
+++ LinkedList/Fundamentals of SLL/05- Deletion of the Kth element of Linked List.java
@@ -25,3 +25,10 @@ class Solution {
 
 // Time Complexity: O(N) worst case, when deleting the tail and O(1) best case, when deleting the head.
 // Space Complexity: O(1) no extra space used.
+
+
+   
+// Interview Follow-ups
+
+// 01- How would you modify this for a circular doubly linked list?
+// 02- How would you modify this to remove all occurrences of a specific value instead of a position?
