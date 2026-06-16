a2be3ae6c17227334a2b347af8c0733986c5cf13
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-06-16 15:28:21 +0530

    Update 03- Deletion of the tail of Linked List.java

--- LinkedList/Fundamentals of SLL/03- Deletion of the tail of Linked List.java
+++ LinkedList/Fundamentals of SLL/03- Deletion of the tail of Linked List.java
@@ -1,1 +1,16 @@
 
+class Solution {
+    public ListNode deleteTail(ListNode head) {
+        //your code goes here
+        if(head==null || head.next == null ) return null;
+        ListNode temp = head;
+        while(temp.next.next!=null){
+            temp=temp.next;
+        }
+        temp.next=null;
+        return head;
+    }
+}
+
+// Time Complexity: O(N) for traversing the linked list and updating the tail.
+// Space Complexity: O(1) as no extra space has been used.
