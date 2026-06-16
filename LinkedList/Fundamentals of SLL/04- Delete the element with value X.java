c8548d66b68648ce1723f5f924d02b2f0325df61
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-06-16 16:07:02 +0530

    Create 04- Delete the element with value X.java

---
+++ LinkedList/Fundamentals of SLL/04- Delete the element with value X.java
@@ -0,0 +1,13 @@
+
+
+
+
+// Interview Follow-ups
+
+// 01- What if you need to delete all occurrences of X? (NOT YET)
+// 02- How would you handle circular linked lists?   (NOT YET)
+
+// 01- For a circular linked list, ensure that the tail node’s next pointer is updated appropriately 
+//     if the deleted node is the head or the node with value X.
+// 02- For a circular linked list, ensure that the tail node’s next pointer is updated appropriately if the deleted node is the head 
+//     or the node with value X.
