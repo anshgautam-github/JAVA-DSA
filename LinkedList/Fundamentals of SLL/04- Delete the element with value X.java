cc234945ca59c79be07eb145bc922958354dead3
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-06-16 16:11:46 +0530

    Update 04- Delete the element with value X.java

--- LinkedList/Fundamentals of SLL/04- Delete the element with value X.java
+++ LinkedList/Fundamentals of SLL/04- Delete the element with value X.java
@@ -18,6 +18,33 @@ class Solution {
 }
 
 
+// ALTERNATE solution -> 
+
+class Solution {
+    public ListNode deleteNodeWithValueX(ListNode head, int X) {
+        if (head == null) return head;
+        
+        if (head.data == X) {
+            head = head.next;
+            return head;
+        }
+
+        ListNode temp = head;
+        ListNode prev = null;
+
+        while (temp != null) {
+            if (temp.data == X) {
+                prev.next = temp.next;
+                return head;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return head;
+    }
+}
+
+
 // Time Complexity: O(N) worst case, when the value is found at the tail, and O(1) best case, 
 //                   when the value is found at the head. Here N is the length of the linked list.
 // Space Complexity: O(1) as no extra space used.
