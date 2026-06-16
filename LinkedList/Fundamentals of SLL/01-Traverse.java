8c30b6e520fd9334bf05a9ffd9ab39fcb6adc01c
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-06-16 15:02:10 +0530

    Update 01-Traverse.java

--- LinkedList/Fundamentals of SLL/01-Traverse.java
+++ LinkedList/Fundamentals of SLL/01-Traverse.java
@@ -13,3 +13,15 @@ class Solution {
     
 // Time Complexity: O(N) since we are visiting each node once. Here N represents the length of the linked list 
 // Space Complexity: O(1) (Auxiliary Space). The algorithm only uses a constant amount of extra space for the traversal pointer
+
+
+// Interview Follow-ups
+
+// 01 -  What if you need to reverse the traversal order?
+// 02 - Can this algorithm be parallelized?
+
+// 01 - Use a stack to store the values during traversal and pop elements from the stack to get them in reverse order. 
+//     Alternatively, reverse the array after traversal.
+
+// 02 - Traversing a singly linked list cannot be parallelized because each node depends on the next pointer of the previous node. 
+//      However, parallelization is possible for tasks performed on the data after traversal.
