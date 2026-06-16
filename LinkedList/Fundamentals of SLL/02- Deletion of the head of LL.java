b88a32d502f85aa261f4b0d9f408d03bbd81f6b1
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-06-16 15:15:56 +0530

    Create 02- Deletion of the head of LL.java

---
+++ LinkedList/Fundamentals of SLL/02- Deletion of the head of LL.java
@@ -0,0 +1,9 @@
+class Solution {
+    public ListNode deleteHead(ListNode head) {
+        //your code goes here
+        if (head==null) return head;
+        head=head.next;
+        return
+    }
+}
+
