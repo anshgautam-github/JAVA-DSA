0e0d36220397e3fd4f9e466e93477f81b4d8a5ad
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-06-17 16:06:49 +0530

    Create 09-Insert node before (kth node) in Doubly Linked List.java

---
+++ LinkedList/DLL Problems/09-Insert node before (kth node) in Doubly Linked List.java
@@ -0,0 +1,37 @@
+
+   class Solution {
+    public ListNode insertBeforeKthPosition(ListNode head, int X, int K) {
+
+        if (K == 1) {
+            ListNode nn = new ListNode(X);
+            nn.next = head;
+
+            if (head != null)
+                head.prev = nn;
+
+            return nn;
+        }
+
+        int count = 1;
+        ListNode temp = head;
+
+        while (temp != null) {
+            if (count == K) break;
+            count++;
+            temp = temp.next;
+        }
+
+        if (temp == null) return head; // K exceeds length
+
+        ListNode prev = temp.prev;
+        ListNode nn = new ListNode(X);
+
+        prev.next = nn;
+        nn.prev = prev;
+
+        nn.next = temp;
+        temp.prev = nn;
+
+        return head;
+    }
+}
