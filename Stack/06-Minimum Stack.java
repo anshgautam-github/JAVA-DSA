8b771e3a93ea62ad4ffc18847cb15fea612cea4b
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-06-18 15:18:20 +0530

    Update 06-Minimum Stack.java

--- Stack/06-Minimum Stack.java
+++ Stack/06-Minimum Stack.java
@@ -1,1 +1,49 @@
 
+import java.util.Stack;
+
+class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+
+        stack.push(val);
+
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+
+        if (stack.isEmpty()) {
+            return;
+        }
+        int removed = stack.pop();
+        if (removed == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+
+        if (stack.isEmpty()) {
+            return -1;
+        }
+        return stack.peek();
+    }
+
+    public int getMin() {
+
+        if (minStack.isEmpty()) {
+            return -1;
+        }
+        return minStack.peek();
+    }
+}
