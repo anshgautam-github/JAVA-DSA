6acb88ef35269ce048fcab0ab493c4defb10fff4
Ansh Gautam <anshgautam1011@gmail.com>
2026-08-23 15:01:13 +0530

    updated files

--- Array/Easy/01-SumOfArray.java
+++ Array/Easy/01-SumOfArray.java
@@ -1,10 +1,18 @@
-public  int sum(int arr[], int n) {
-      int sum = 0;
-      for(int i =0;i<n;i++){
-        sum+=arr[i];
-      }
-      return sum;
-}
+public class Main {
 
+    public static int sum(int arr[], int n) {
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int result = sum(arr, arr.length);
+        System.out.println(result);
+    }
+}
 // Time Complexity : O(N), because each element in the array is processed exactly once.
 // Space Complexity : O(1), because only couple of variable are used.
