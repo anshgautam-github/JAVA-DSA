df0d23096a0e5edb641498cded05ba9d7fd92bde
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-09-01 20:42:07 +0530

    Create 08-Sort Characters by Frequency.java

---
+++ Strings/Easy/08-Sort Characters by Frequency.java
@@ -0,0 +1,12 @@
+// You are given a string s. Return the array of unique characters, sorted by highest to lowest occurring characters.
+// If two or more characters have same frequency then arrange them in alphabetic order.
+
+// Input : s = "tree"
+// Output : ['e', 'r', 't' ]
+// Explanation : The occurrences of each character are as shown below :
+// e --> 2
+// r --> 1
+// t --> 1.
+// The r and t have same occurrences , so we arrange them by alphabetic order.
+
+
