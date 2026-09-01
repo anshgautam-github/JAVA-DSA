2c8122d2f0f8ec038c361c9b0657c67a9a0164bd
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-09-02 02:33:40 +0530

    Update 08-fib.java

--- Recursion/Easy/08-fib.java
+++ Recursion/Easy/08-fib.java
@@ -5,3 +5,8 @@
         // Recursive case: F(n) = F(n-1) + F(n-2)
         return fib(n - 1) + fib(n - 2);
     }
+
+
+// Time Complexity O(2^N) — Each function call makes two more calls (for n-1 and n-2), resulting in an exponential growth in the number of calls.
+
+// Space Complexity O(N)— The call stack grows with each recursive call, using N stack frames, so the space complexity is proportional to the recursion depth.
